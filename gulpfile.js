a30c5b4217170f88d17c8cb00664b9d90c141368
petrickad <petrick.duarte99@gmail.com>
2024-12-09 13:24:33 -0300

    adicionando outras dependencias

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,20 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 const imagemin = require('gulp-imagemin');
+const  uglify = require('gulp-uglify');
+const obfuscate = require('gulp-obfuscate');
 
 function styles() {
     return gulp.src('./src/styles/*.scss')
         .pipe(sass({ outputStyle: 'compressed'}))
         .pipe(gulp.dest('./dist/css'));
+}
+
+function comprimeJavaScript() {
+    return gulp.src('./src/*.js')
+        .pipe(uglify())
+        .pipe(obfuscate())
+        .pipe(gulp.dest('./dist/scripts'));
 }
 
 function images() {
@@ -14,7 +23,7 @@ function images() {
         .pipe(gulp.dest('./dist/images'));
 }
 
-exports.default = gulp.parallel(styles, images);
+exports.default = gulp.parallel(styles, images, comprimeJavaScript);
  exports.watch = function(){
     gulp.watch('./src/styles/*.scss', gulp.parallel(styles));
  }
